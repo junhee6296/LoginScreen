ceab5d86701977265002db4a178a87096f1c605f
Rhythmfactory <64122960+junhee6296@users.noreply.github.com>
2026-04-02 11:10:50 +0900

    1단계 과제 수행중 아이디/비밀번호 박스 배치 및 글자 입력/사라짐 추

--- Form1.Designer.cs
+++ Form1.Designer.cs
@@ -1,6 +1,6 @@
 ﻿namespace LoginScreen
 {
-    partial class Form1
+    partial class LoginMainForm
     {
         /// <summary>
         ///  Required designer variable.
@@ -28,12 +28,78 @@
         /// </summary>
         private void InitializeComponent()
         {
-            components = new System.ComponentModel.Container();
+            lblAppName = new Label();
+            txtID = new TextBox();
+            txtPW = new TextBox();
+            btnLogin = new Button();
+            SuspendLayout();
+            // 
+            // lblAppName
+            // 
+            lblAppName.AutoSize = true;
+            lblAppName.Font = new Font("서울남산체 L", 48F, FontStyle.Regular, GraphicsUnit.Point, 129);
+            lblAppName.Location = new Point(188, 22);
+            lblAppName.Name = "lblAppName";
+            lblAppName.Size = new Size(189, 76);
+            lblAppName.TabIndex = 0;
+            lblAppName.Text = "Login";
+            // 
+            // txtID
+            // 
+            txtID.Font = new Font("서울한강 장체 L", 27.7499962F);
+            txtID.ForeColor = SystemColors.ScrollBar;
+            txtID.Location = new Point(40, 121);
+            txtID.Name = "txtID";
+            txtID.Size = new Size(493, 51);
+            txtID.TabIndex = 1;
+            txtID.Text = "아이디";
+            txtID.Enter += txtID_Enter;
+            txtID.Leave += txtID_Leave;
+            // 
+            // txtPW
+            // 
+            txtPW.Font = new Font("서울한강 장체 L", 27.7499962F);
+            txtPW.ForeColor = SystemColors.ScrollBar;
+            txtPW.Location = new Point(40, 194);
+            txtPW.Name = "txtPW";
+            txtPW.Size = new Size(493, 51);
+            txtPW.TabIndex = 2;
+            txtPW.Text = "비밀번호";
+            txtPW.Enter += txtPW_Enter;
+            txtPW.Leave += txtPW_Leave;
+            // 
+            // btnLogin
+            // 
+            btnLogin.BackColor = SystemColors.ActiveCaption;
+            btnLogin.Font = new Font("서울남산체 M", 27.7499962F, FontStyle.Bold, GraphicsUnit.Point, 129);
+            btnLogin.ForeColor = SystemColors.ButtonFace;
+            btnLogin.Location = new Point(204, 272);
+            btnLogin.Name = "btnLogin";
+            btnLogin.Size = new Size(173, 64);
+            btnLogin.TabIndex = 3;
+            btnLogin.Text = "로그인";
+            btnLogin.UseVisualStyleBackColor = false;
+            // 
+            // LoginMainForm
+            // 
+            AutoScaleDimensions = new SizeF(7F, 15F);
             AutoScaleMode = AutoScaleMode.Font;
-            ClientSize = new Size(800, 450);
-            Text = "Form1";
+            ClientSize = new Size(574, 371);
+            Controls.Add(btnLogin);
+            Controls.Add(txtPW);
+            Controls.Add(txtID);
+            Controls.Add(lblAppName);
+            Name = "LoginMainForm";
+            Text = "로그인";
+            ResumeLayout(false);
+            PerformLayout();
         }
 
         #endregion
+
+        private Label lblAppName;
+        private TextBox txtID;
+        private TextBox txtPW;
+        private Button btnLogin;
     }
 }
--- Form1.cs
+++ Form1.cs
@@ -1,10 +1,50 @@
 namespace LoginScreen
 {
-    public partial class Form1 : Form
+    public partial class LoginMainForm : Form
     {
-        public Form1()
+        public LoginMainForm()
         {
             InitializeComponent();
+        }
+
+        // 아이디 가이드텍스트 on/off
+        private void txtID_Enter(object sender, EventArgs e)
+        {
+            if (txtID.Text == "아이디")
+            {
+                txtID.Text = "";
+                txtID.ForeColor = Color.Black;
+            }
+        }
+
+        private void txtID_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtID.Text))
+            {
+                txtID.Text = "아이디";
+                txtID.ForeColor = Color.Silver;
+            }
+        }
+
+        // 비밀번호 가이드텍스트 on/off
+        private void txtPW_Enter(object sender, EventArgs e)
+        {
+            if (txtPW.Text == "비밀번호")
+            {
+                txtPW.Text = "";
+                txtPW.ForeColor = Color.Black;
+                txtPW.UseSystemPasswordChar = true;
+            }
+        }
+
+        private void txtPW_Leave(object sender, EventArgs e)
+        {
+            if (string.IsNullOrWhiteSpace(txtPW.Text))
+            {
+                txtPW.UseSystemPasswordChar = false;
+                txtPW.Text = "비밀번호";
+                txtPW.ForeColor = Color.Silver;
+            }
         }
     }
 }
